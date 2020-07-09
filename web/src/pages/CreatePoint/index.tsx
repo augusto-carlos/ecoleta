07deac1f612401bdedb73c33d797ae8d7b752458
Augusto Carlos <augustocarlos9966@gmail.com>
2020-07-09 19:04:30 +0100

    creating a dropzone

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -4,8 +4,9 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
-
 import api from '../../services/api';
+
+import Dropzone from '../../components/Dropzone';
 
 import './styles.css';
 
@@ -26,17 +27,15 @@ interface IBGECityResponse {
 }
 
 const CreatePoint: React.FC = () => {
-
   const [items, setItems] = useState<Item[]>([]);
-  const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
   const [ufs, setUfs] = useState<string[]>([]);
-  const [selectedUf, setSelectedUf] = useState('0');
   const [cities, setCities] = useState<string[]>([]);
-  const [selectedCity, setSelectedCity] = useState('0');
 
-  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
-  const [initialMapPosition, setInitialMapPosition] = useState<[number, number]>([0, 0]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
 
   const [formData, setFormData] = useState({
     name: '',
@@ -44,69 +43,88 @@ const CreatePoint: React.FC = () => {
     whatsapp: '',
   });
 
-  const history = useHistory();
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
+    0,
+    0,
+  ]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
-      setInitialMapPosition([latitude, longitude]);
+
+      setInitialPosition([latitude, longitude]);
     });
   }, []);
 
+  const history = useHistory();
+
   useEffect(() => {
-    api.get('items').then(response => {
+    api.get('items').then((response) => {
       setItems(response.data);
     });
   }, []);
 
   useEffect(() => {
     axios
-      .get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-      .then(response => {
-        const ufInitials = response.data.map(uf => uf.sigla);
+      .get<IBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome'
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+
         setUfs(ufInitials);
       });
   }, []);
 
   useEffect(() => {
-    if (selectedUf === '0') {
-      return;
-    }
+    if (selectedUf === '0') return;
+
     axios
-      .get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
-      .then(response => {
-        const cityNames = response.data.map(city => city.nome);
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+
         setCities(cityNames);
       });
   }, [selectedUf]);
 
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
+
     setSelectedUf(uf);
   }
 
   function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
     const city = event.target.value;
+
     setSelectedCity(city);
   }
 
   function handleMapClick(event: LeafletMouseEvent) {
-    setSelectedPosition([
-      event.latlng.lat,
-      event.latlng.lng
-    ])
+    setSelectedPosition([event.latlng.lat, event.latlng.lng]);
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   }
 
   function handleSelectItem(id: number) {
-    const alreadSelected = selectedItems.findIndex(item => item === id);
+    const alreadySelected = selectedItems.findIndex((item) => item === id);
 
-    if (alreadSelected >= 0) {
-      const filteredItems = selectedItems.filter(item => item !== id);
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id);
+
       setSelectedItems(filteredItems);
     } else {
       setSelectedItems([...selectedItems, id]);
@@ -122,20 +140,24 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('points', data);
 
-    alert('Ponto de Coleta Criado!');
+    alert('Ponto de coleta criado!');
 
     history.push('/');
   }
@@ -143,16 +165,20 @@ const CreatePoint: React.FC = () => {
   return (
     <div id="page-create-point">
       <header>
-        <img src={logo} alt="Ecoleta Logo" />
+        <img src={logo} alt="Ecoleta" />
 
         <Link to="/">
           <FiArrowLeft />
-          Voltar para home
+          Voltar para a home
         </Link>
       </header>
 
       <form onSubmit={handleSubmit}>
-        <h1>Cadastro do <br />ponto de coleta</h1>
+        <h1>
+          Cadastro do <br /> ponto de coleta
+        </h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
@@ -171,7 +197,7 @@ const CreatePoint: React.FC = () => {
 
           <div className="field-group">
             <div className="field">
-              <label htmlFor="email">E-mail</label>
+              <label htmlFor="email">Email</label>
               <input
                 type="email"
                 name="email"
@@ -179,6 +205,7 @@ const CreatePoint: React.FC = () => {
                 onChange={handleInputChange}
               />
             </div>
+
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
               <input
@@ -197,11 +224,12 @@ const CreatePoint: React.FC = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={initialMapPosition} zoom={15} onClick={handleMapClick}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
             <Marker position={selectedPosition} />
           </Map>
 
@@ -215,11 +243,14 @@ const CreatePoint: React.FC = () => {
                 onChange={handleSelectUf}
               >
                 <option value="0">Selecione uma UF</option>
-                {ufs.map(uf => (
-                  <option key={uf} value={uf}>{uf}</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
                 ))}
               </select>
             </div>
+
             <div className="field">
               <label htmlFor="city">Cidade</label>
               <select
@@ -229,8 +260,10 @@ const CreatePoint: React.FC = () => {
                 onChange={handleSelectCity}
               >
                 <option value="0">Selecione uma cidade</option>
-                {cities.map(city => (
-                  <option key={city} value={city}>{city}</option>
+                {cities.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
                 ))}
               </select>
             </div>
@@ -239,12 +272,12 @@ const CreatePoint: React.FC = () => {
 
         <fieldset>
           <legend>
-            <h2>Ítens de coleta</h2>
-            <span>Selecione um ou mais ítens abaixo</span>
+            <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
           </legend>
 
           <ul className="items-grid">
-            {items.map(item => (
+            {items.map((item) => (
               <li
                 key={item.id}
                 onClick={() => handleSelectItem(item.id)}
@@ -257,9 +290,7 @@ const CreatePoint: React.FC = () => {
           </ul>
         </fieldset>
 
-        <button type="submit">
-          Cadastrar ponto de coleta
-        </button>
+        <button type="submit">Cadastrar ponto de coleta</button>
       </form>
     </div>
   );
